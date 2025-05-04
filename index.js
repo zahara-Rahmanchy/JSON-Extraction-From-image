ae9b7ae488f91389461dcf6849c89d347e679d1e
zahara-Rahmanchy <zaharachy72@gmail.com>
2025-05-04 14:09:27 +0600

    cors updated

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ const corsOptions = {
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE',"OPTIONS"],
   }));
-  app.options('/*', cors(corsOptions));
+  // app.options('/*', cors(corsOptions));
 
 // === Routes ===
 app.get('/', (req, res) => {
