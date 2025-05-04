3f5f829504e8dbeb061bd07197e51f7c79822713
zahara-Rahmanchy <zaharachy72@gmail.com>
2025-05-04 14:06:05 +0600

    vercel updated

--- index.js
+++ index.js
@@ -120,7 +120,8 @@ const corsOptions = {
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE',"OPTIONS"],
   }));
-  app.options('*', cors(corsOptions));
+  app.options('/*', cors(corsOptions));
+
 // === Routes ===
 app.get('/', (req, res) => {
   res.send('Hello from JSON Extraction API!');
